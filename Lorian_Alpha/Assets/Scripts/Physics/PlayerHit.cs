5f6975555618555704dccaf2b235f0a8cb89b2ef
Keynan <80490966+KeynanProgramming@users.noreply.github.com>
2022-11-10 12:10:59 -0300

    Elevator

--- Lorian_Alpha/Assets/Scripts/Physics/PlayerHit.cs
+++ Lorian_Alpha/Assets/Scripts/Physics/PlayerHit.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class PlayerHit : MonoBehaviour
 {
+    public GameObject Player;
+    public Transform teleport;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Statue"))
         {
             collision.GetComponent<Statue>().OnRestore();
+        }
+
+        if (collision.CompareTag("Elevator"))
+        {
+            Player.transform.position = teleport.transform.position;
+           
         }
     }
 }
