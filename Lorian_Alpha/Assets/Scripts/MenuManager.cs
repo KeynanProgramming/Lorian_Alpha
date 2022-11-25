d18e12ed9bd426d4660c5996fb5ca27783e7c8f9
XLordDeusX <wilfredoa.rukoz@gmail.com>
2022-11-25 12:56:14 -0300

    Main menu updates

--- Lorian_Alpha/Assets/Scripts/MenuManager.cs
+++ Lorian_Alpha/Assets/Scripts/MenuManager.cs
@@ -2,22 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class MenuManager : MonoBehaviour
 {
-    public GameObject fadeFromBlack;
+    public GameObject fadeFromBlack, backControls, controlsButton, backCredits, creditsButton;
+
     private void Start()
     {
         if (fadeFromBlack != null)
         {
             GameObject panel = Instantiate(fadeFromBlack, Vector3.zero, Quaternion.identity) as GameObject;
-            Destroy(panel, 1);
+            Destroy(panel, 1.5f);
         }
     }
 
     public void Play ()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void OpenControls()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(backControls);
+    }
+
+    public void CloseControls()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(controlsButton);
+    }
+
+    public void OpenCredits()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(backCredits);
+    }
+
+    public void CloseCredits()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(creditsButton);
     }
 
     public void Exit()
