ef4576ec6e920c5cdcd072e03f9462d5ad09a0bc
XLordDeusX <wilfredoa.rukoz@gmail.com>
2022-11-26 15:38:03 -0300

    No movement with dialogs

--- Lorian_Alpha/Assets/Scripts/Blocker.cs
+++ Lorian_Alpha/Assets/Scripts/Blocker.cs
@@ -5,37 +5,36 @@ using UnityEngine.UI;
 
 public class Blocker : MonoBehaviour
 {
-    public GameObject dialogBox;
+    public GameObject dialogBox, hero;
     public Text dialogText;
     public string dialog;
     public bool playerInRange;
 
     void Update()
     {
-        if(playerInRange == true && Input.GetKeyDown(KeyCode.Space))
+        if (dialogBox.activeInHierarchy)
+        {
+            hero.GetComponent<Lorian>().movement = Vector2.zero;
+        }
+
+        if (playerInRange == true && Input.GetKeyDown(KeyCode.Space))
         {
             if(dialogBox.activeInHierarchy)
             {
                 dialogBox.SetActive(false);
-            }
-            else
-            {
-                dialogBox.SetActive(true);
-                dialogText.text = dialog;
+                hero.GetComponent<Lorian>().movement.x = Input.GetAxisRaw("Horizontal");
+                hero.GetComponent<Lorian>().movement.y = Input.GetAxisRaw("Vertical");
             }
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        collision.gameObject.GetComponent<Lorian>().moveSpeed = 0;
-
         if (collision.gameObject.CompareTag("Player"))
         {
             playerInRange = true;
             dialogBox.SetActive(true);
             dialogText.text = dialog;
-            
         }
     }
 
