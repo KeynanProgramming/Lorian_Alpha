5efccfc75f60501d483af4e9a99bce7201a78b7c
Keynan <80490966+KeynanProgramming@users.noreply.github.com>
2022-11-28 05:21:58 -0300

    Cinematic

--- Lorian_Alpha/Assets/Scripts/CinematicManager.cs
+++ Lorian_Alpha/Assets/Scripts/CinematicManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CinematicManager : MonoBehaviour
 {
-    public GameObject fadeFromBlack;
-    
+    public GameObject fadeFromBlack, fadeToBlack, fadeToWhite,fadeInOut;
+    public GameObject panel0, panel1, panel2, panel3;
+    private int i;
     void Start()
     {
         if (fadeFromBlack != null)
@@ -17,9 +19,43 @@ public class CinematicManager : MonoBehaviour
 
     void Update()
     {
+        if (i > 3)
+        {
+            GameObject panel = Instantiate(fadeToBlack, Vector3.zero, Quaternion.identity);
+            StartCoroutine(fadeToBlackCo());
+            SceneManager.LoadScene(2);
+        }
         if (Input.GetKeyDown(KeyCode.Space))
         {
-
+            GameObject panel = Instantiate(fadeFromBlack, Vector3.zero, Quaternion.identity);
+            //StartCoroutine(fadeFromBlackCo());                
+            i++;
+            switch (i)
+            {               
+                case 1:
+                    panel= Instantiate(fadeFromBlack, Vector3.zero, Quaternion.identity);
+                    Destroy(panel, 1.5f); ;
+                    panel0.SetActive(false);
+                    panel1.SetActive(true);                   
+                    break;
+                case 2:
+                    panel = Instantiate(fadeFromBlack, Vector3.zero, Quaternion.identity);
+                    panel1.SetActive(false);
+                    panel2.SetActive(true);                    
+                    break;
+                case 3:
+                    panel = Instantiate(fadeFromBlack, Vector3.zero, Quaternion.identity);
+                    panel2.SetActive(false);
+                    panel3.SetActive(true);                   
+                    break;
+                default:
+                    break;
+            }
         }
     }
+    private IEnumerator fadeToBlackCo()
+    {
+        yield return new WaitForSeconds(2);
+    }
+                
 }
